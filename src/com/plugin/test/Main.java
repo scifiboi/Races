c6390f1a0af481b7dc1045cbd80c6b832a5c82c7
scifiboi <scifiboi@yahoo.com>
2014-03-22 22:26:30 -0500

    Lots of fixes

--- src/com/plugin/test/Main.java
+++ src/com/plugin/test/Main.java
@@ -3,20 +3,21 @@ package com.plugin.test;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
-import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
      
 public final class Main extends JavaPlugin {
 
+    @Override
     public void onEnable(){
         getLogger().info("Disabled!");
     }
  
+    @Override
     public void onDisable(){
         getLogger().info("Enabled!");
     }
     
+    @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args){
         CommandSender player = sender;
         if(cmd.getName().equalsIgnoreCase("race"))
@@ -26,6 +27,7 @@ public final class Main extends JavaPlugin {
                 case (0):
                 {
                     player.sendMessage(ChatColor.RED + "Usage: /race {get/set/list}");
+                    break;
                 }
                 case (1):
                 {
@@ -39,14 +41,25 @@ public final class Main extends JavaPlugin {
                             player.sendMessage(" - Dwarf");
                             player.sendMessage(" - Orc");
                             player.sendMessage(" - (Later on) Gnome");
+                            break;
                         }
                         case ("get"):
+                        {
                             player.sendMessage(ChatColor.RED + "Usage: /race get [player]");
+                            break;
+                        }
                         case ("set"):
+                        {
                             player.sendMessage(ChatColor.RED + "Usage: /race set [player] [race]");
+                            break;
+                        }
                         default:
+                        {
                             player.sendMessage(ChatColor.RED + "Usage: /race {get/set/list} [player] [race]");
+                            break;
+                        }
                     }
+                    break;
                 }
             }
         }
